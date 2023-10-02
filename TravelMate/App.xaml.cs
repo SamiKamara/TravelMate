41a504ee25f89517d7023c0b94126518aa7865db
Sami Nikkanen <sami.nikkanen@tuni.fi>
2023-10-02 15:41:45 +0300

    feat: add global backgroundcolor

--- TravelMate/App.xaml.cs
+++ TravelMate/App.xaml.cs
@@ -6,9 +6,20 @@
         {
             InitializeComponent();
 
-            //MainPage = new MainPage();
-
             MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 1024;
+            const int newHeight = 576;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
         }
     }
 }
