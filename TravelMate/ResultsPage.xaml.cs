c559e34332d583034a1ae576ddd60b879480ed70
Sami Nikkanen <sami.nikkanen@tuni.fi>
2023-10-24 17:25:53 +0300

    feat: initial digitransit integration

--- TravelMate/ResultsPage.xaml.cs
+++ TravelMate/ResultsPage.xaml.cs
@@ -1,15 +1,41 @@
-namespace TravelMate;
+using Microsoft.Maui.Controls;
+using Newtonsoft.Json.Linq;
 
-public partial class ResultsPage : ContentPage
+namespace TravelMate
 {
-	public ResultsPage()
-	{
-		InitializeComponent();
-	}
-
-    protected override void OnAppearing()
+    public partial class ResultsPage : ContentPage
     {
-        base.OnAppearing();
-        NavigationPage.SetHasNavigationBar(this, false);
+        public ResultsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnGetRouteClicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(StartCoords.Text) || string.IsNullOrEmpty(EndCoords.Text))
+            {
+                await DisplayAlert("Error", "Please enter both start and end coordinates.", "OK");
+                return;
+            }
+
+            string[] startCoords = StartCoords.Text.Split(',');
+            string[] endCoords = EndCoords.Text.Split(',');
+
+            JObject route = await DigitransitHelper.GetRoute(
+                double.Parse(startCoords[0].Trim()),
+                double.Parse(startCoords[1].Trim()),
+                double.Parse(endCoords[0].Trim()),
+                double.Parse(endCoords[1].Trim())
+            );
+
+            // For demonstration purposes, display the raw JSON.
+            RouteLabel.Text = route.ToString();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
     }
 }
---
+++ TravelMate/Utilities/DigitransitHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+public static class DigitransitHelper
+{
+    private const string BaseUrl = "https://api.digitransit.fi/routing/v1/routers/finland/index/graphql";
+    private const string SubscriptionKey = "235c8c6202c045078074f901deecd65f";
+    private static readonly HttpClient httpClient = new HttpClient();
+
+    public static async Task<JObject> GetRoute(double startLat, double startLng, double endLat, double endLng)
+    {
+        string query = $@"
+    {{
+        plan(from: {{lat: {startLat}, lon: {startLng}}}, 
+             to: {{lat: {endLat}, lon: {endLng}}}, 
+             numItineraries: 3) 
+        {{
+            itineraries 
+            {{
+                legs 
+                {{
+                    startTime
+                    endTime
+                    mode
+                    duration
+                    realTime
+                    distance
+                    transitLeg
+                }}
+            }}
+        }}
+    }}";
+
+        var requestPayload = new StringContent(JsonConvert.SerializeObject(new { query = query }), Encoding.UTF8, "application/json");
+        httpClient.DefaultRequestHeaders.Add("digitransit-subscription-key", SubscriptionKey);
+
+        var response = await httpClient.PostAsync(BaseUrl, requestPayload);
+        string jsonResponse = await response.Content.ReadAsStringAsync();
+
+        return JObject.Parse(jsonResponse);
+    }
+}
