295bbb602d576840ec9b071cd7899acc59421549
Sami Nikkanen <sami.nikkanen@tuni.fi>
2023-11-15 10:50:05 +0200

    feat: make results print on appearing

--- TravelMate/Views/ResultsPage.xaml.cs
+++ TravelMate/Views/ResultsPage.xaml.cs
@@ -33,6 +33,31 @@ namespace TravelMate
             await viewModel.GetWeatherAsync(ResultEditor, StreetAddressInput.Text);
         }
 
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            NavigationPage.SetHasNavigationBar(this, false);
+            HideAllExceptResultEditor();
+            _ = OnResultsPageLoadedAsync();
+        }
+
+        private async Task OnResultsPageLoadedAsync()
+        {
+            try
+            {
+                await OnResultsPageLoaded();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+
+        private async Task OnResultsPageLoaded()
+        {
+            await viewModel.GetRouteAsync(ResultEditor, DesiredWeather, DestinationWeather);
+        }
+
         private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
         {
             if (!int.TryParse(e.NewTextValue, out int result))
@@ -45,6 +70,18 @@ namespace TravelMate
             {
                 ((Entry)sender).Text = e.OldTextValue;
             }
+        }
+
+        // These will be removed entirely once not needed for debugging
+        private void HideAllExceptResultEditor()
+        {
+            tempEntry.IsVisible = false;
+            rainEntry.IsVisible = false;
+            cloudsEntry.IsVisible = false;
+            windEntry.IsVisible = false;
+            StartCoords.IsVisible = false;
+            EndCoords.IsVisible = false;
+            StreetAddressInput.IsVisible = false;
         }
     }
 }
