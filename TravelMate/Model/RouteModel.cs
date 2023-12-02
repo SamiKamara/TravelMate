2b6ffd78bcd2233e65144d48ceb7a36656315462
kassq <kasperi.pohtine@gmail.com>
2023-12-03 01:14:23 +0200

    Calculate match percentages

--- TravelMate/Model/RouteModel.cs
+++ TravelMate/Model/RouteModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -69,6 +70,70 @@ namespace TravelMate.Model
         public string FormattedDate { get => formattedDate;  set => formattedDate = value; }
         public TimeSpan TotalTravelTime { get => totalTravelTime;  set => totalTravelTime = value; }
         public string FormattedTotalTravelTime { get => formattedTotalTravelTime; private set => formattedTotalTravelTime = value; }
+
+        public int RainChanceMatchPercentage
+        {
+            get
+            {
+                return CalculatePercentageDifference(InputRainChance, ResultRainChance);
+            }
+        }
+
+        public int CloudinessMatchPercentage
+        {
+            get
+            {
+                return CalculatePercentageDifference(InputCloudiness, ResultCloudiness);
+            }
+        }
+
+        private readonly double toleranceForWindSpeed = 10.0; // If over 10 m/s, the match percentage is 0
+        private readonly double toleranceForTemperature = 20.0; // If over 20 degrees, the match percentage is 0
+
+        public int WindSpeedMatchPercentage
+        {
+            get
+            {
+                return CalculatePercentageCloseness(InputWindSpeed, ResultWindSpeed, toleranceForWindSpeed);
+            }
+        }
+
+        public int TemperatureMatchPercentage
+        {
+            get
+            {
+                return CalculatePercentageCloseness(InputTemperature, ResultTemperature, toleranceForTemperature);
+            }
+        }
+
+        static int CalculatePercentageCloseness(double inputValue, double resultValue, double threshold)
+        {
+            // Calculate the absolute difference between the temperatures
+            double difference = Math.Abs(inputValue - resultValue);
+
+            // Calculate the percentage closeness based on the difference and threshold
+            double percentageCloseness = 100.0 - (difference / threshold) * 100.0;
+
+            // Ensure the result is within the range [0, 100] and convert to integer
+            int result = (int)Math.Max(0, Math.Min(100, percentageCloseness));
+
+            return result;
+        }
+
+        static int CalculatePercentageDifference(int percentage1, int percentage2)
+        {
+            // Ensure the percentages are within the valid range (0-100)
+            percentage1 = Math.Max(0, Math.Min(100, percentage1));
+            percentage2 = Math.Max(0, Math.Min(100, percentage2));
+
+            // Calculate the absolute difference
+            int absoluteDifference = Math.Abs(percentage1 - percentage2);
+
+            // Calculate the adjusted percentage difference
+            int adjustedPercentageDifference = 100 - absoluteDifference;
+
+            return adjustedPercentageDifference;
+        }
 
         public void CalculateTotalTravelTime(DateTimeOffset? startTime, DateTimeOffset? endTime)
         {
